02cd83487498b3124609172cf987a6d527d7517d
TatiYas <iasinovska_lawyer@ukr.net>
2025-12-23 12:10:36 +0100

    Update SearchBar.tsx

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -23,3 +23,34 @@ function SearchBar({ onSubmit }: SearchBarProps) {
     };
    
  
+
+    return (
+    <header className={styles.header}>
+        <div className={styles.container}>
+            <a
+                className={styles.link}
+                href="https://www.themoviedb.org/"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                Powered by TMDB
+            </a>
+            <form className={styles.form} action={handleSubmit}>
+                <input
+                    className={styles.input}
+                    type="text"
+                    name="query"
+                    autoComplete="off"
+                    placeholder="Search movies..."
+                    autoFocus
+                />
+                <button className={styles.button} type="submit">
+                    Search
+                </button>
+                </form>
+                <Toaster position="top-center" />
+        </div>
+    </header>)
+}
+
+export default SearchBar
